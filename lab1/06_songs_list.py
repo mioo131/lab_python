d8832c6fba44834f8f73e39c5ba22fa106526ebf
mioo131 <kustovyy@bk.ru>
2025-02-26 23:03:29 +0500

    Update 06_songs_list.py

--- lab1/06_songs_list.py
+++ lab1/06_songs_list.py
@@ -24,7 +24,8 @@ violator_songs_list = [
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 # TODO здесь ваш код
-
+time = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[-1][1]
+print("Три песни звучат",round(time,3),"минут")
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
     'World in My Eyes': 4.76,
@@ -42,3 +43,5 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+time2 = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+print("А другие три песни звучат ",round(time2,3),"минут")
