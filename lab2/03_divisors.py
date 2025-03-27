c6ef20d649e0291a8bb99124e1b9e27a12437c73
mioo131 <kustovyy@bk.ru>
2025-03-27 14:09:56 +0500

    Update 03_divisors.py

--- lab2/03_divisors.py
+++ lab2/03_divisors.py
@@ -15,13 +15,22 @@ def find_divisors(n):
                 divisors.append(n // i)
     return divisors
 
-if __name__ == "__main__":
+def run_tests():
     import doctest
-    doctest.testmod()  # запускает все тесты из документации
+    res = doctest.testmod()
+    if res.failed == 0:
+        print ('\nВсе доктесты пройдены успешно')
+    else:
+        print ('\nДоктесты не пройдены где-то ошибка')
 
+def main():
+    run_tests()
+    print("\nЧисла с ровно двумя делителями в диапазоне 174457–174505:")  
+    for num in range(174457, 174505 + 1):  
+    # for num in range(5, 9):  
+        divisors = find_divisors(num)  
+        if len(divisors) == 2:  # если два делителя  
+            print(f'Число {num}')
+            print(f"{divisors[0]} {divisors[1]}")
 
-for num in range(174457, 174505+1):
-    divisors = find_divisors(num)
-    if len(divisors) == 2:  # если два делителя
-        divisors.sort()  # сортируем
-        print(f"{divisors[0]} {divisors[1]}")
+main()
